c67f94b12f6005d62f713e3ef2c41e9792e01a03
Brandon Knox <bknox.dev@gmail.com>
2024-06-26 14:26:14 -0700

    Initial Commit

---
+++ PhysicsCharts/PhysicsChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhysicsChartsApp.swift
+//  PhysicsCharts
+//
+//  Created by Brandon Knox on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct PhysicsChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
